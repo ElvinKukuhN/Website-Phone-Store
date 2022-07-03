f51a8e0984ff3c1d2a807df0c5bcbf7437e0ff89
Elvin Kukuh Nurdiyansah <elvinkukuh20@student.uns.ac.id>
2022-07-03 20:06:44 +0700

    Update register.php

--- pages/register.php
+++ pages/register.php
@@ -194,22 +194,22 @@
 
                         <!-- nama input -->
                         <div class="mb-6">
-                            <input type="text" class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-fuchsia-600 focus:outline-none" id="exampleFormControlInput2" placeholder="Name" />
+                            <input type="text" name="nama" class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-fuchsia-600 focus:outline-none" id="exampleFormControlInput2" placeholder="Name" />
                         </div>
 
                         <!-- Email input -->
                         <div class="mb-6">
-                            <input type="text" class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-fuchsia-600 focus:outline-none" id="exampleFormControlInput2" placeholder="Email address" />
+                            <input type="text" name="email" class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-fuchsia-600 focus:outline-none" id="exampleFormControlInput2" placeholder="Email address" />
                         </div>
 
                         <!-- Password input -->
                         <div class="mb-6">
-                            <input type="password" class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-fuchsia-600 focus:outline-none" id="exampleFormControlInput2" placeholder="Password" />
+                            <input type="password" name="password" class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-fuchsia-600 focus:outline-none" id="exampleFormControlInput2" placeholder="Password" />
                         </div>
 
                         <!-- verif Password input -->
                         <div class="mb-6">
-                            <input type="password" class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-fuchsia-600 focus:outline-none" id="exampleFormControlInput2" placeholder="Confirm Password" />
+                            <input type="password" name="password" class="form-control block w-full px-4 py-2 text-xl font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-fuchsia-600 focus:outline-none" id="exampleFormControlInput2" placeholder="Confirm Password" />
                         </div>
 
                         <div class="flex justify-between items-center mb-6">
